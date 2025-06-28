dacf6ce674c2bcf6443ef0dbe8044b374599f1f3
Amal <amal.gabidov@mail.ru>
2025-06-28 18:36:36 +0300

    memoize OffersList

--- src/components/offer-list/offer-list.tsx
+++ src/components/offer-list/offer-list.tsx
@@ -1,22 +1,36 @@
 import { Offer } from '../../types/offer.ts';
 import OfferCard from '../offer-card/offer-card.tsx';
+import React, { useCallback } from 'react';
 
 type OffersListProps = {
   offers: Offer[];
   onOfferHover: (offer: Offer | null) => void;
 };
 
-export default function OffersList({ offers, onOfferHover }: OffersListProps) {
+const OffersList = ({ offers, onOfferHover }: OffersListProps) => {
+  const handleMouseEnter = useCallback(
+    (offer: Offer) => () => onOfferHover(offer),
+    [onOfferHover]
+  );
+
+  const handleMouseLeave = useCallback(
+    () => onOfferHover(null),
+    [onOfferHover]
+  );
+
   return (
     <div className="cities__places-list places__list tabs__content">
       {offers.map((offer) => (
         <OfferCard
-          offer={offer}
           key={offer.id}
-          onMouseEnter={() => onOfferHover(offer)}
-          onMouseLeave={() => onOfferHover(null)}
+          offer={offer}
+          onMouseEnter={handleMouseEnter(offer)}
+          onMouseLeave={handleMouseLeave}
         />
       ))}
     </div>
   );
-}
+};
+
+OffersList.displayName = 'OffersList';
+export default React.memo(OffersList);
